f2d1bb24b21feca5f6b69cf9bb1f4ab3cec472d5
saguiguilid <tbki.jea@gmail.com>
2021-07-17 22:21:02 +0800

    Add gem rolify - add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +8,12 @@ class User < ApplicationRecord
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_many :courses
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    add_role(:student) if roles.blank?
+  end
 
   def to_s
     email
