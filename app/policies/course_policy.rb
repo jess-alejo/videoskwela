271f1d239a10e50f019983347c58067405216658
saguiguilid <tbki.jea@gmail.com>
2021-07-19 10:07:41 +0800

    Add gem pundit - add authorization for courses

---
+++ app/policies/course_policy.rb
@@ -0,0 +1,19 @@
+class CoursePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    @user.has_role?(:teacher)
+  end
+
+  def update?
+    @user.has_role?(:admin) || @user == @record.author
+  end
+
+  def destroy?
+    @user.has_role?(:admin) || @user == @record.author
+  end
+end
