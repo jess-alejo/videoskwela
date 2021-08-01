8d1feb83b97c0c414413863d7e2b1122f6b18919
saguiguilid <tbki.jea@gmail.com>
2021-08-01 17:15:09 +0800

    Make page title dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def active_link(active)
     active ? 'active' : ''
   end
+
+  def page_title(title)
+    content_for :title do
+      "Videoskwela | #{title}"
+    end
+  end
 end
