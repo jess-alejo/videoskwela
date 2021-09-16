75f2fe366b4c94de9cf0d63a8cef85fac83ae121
saguiguilid <tbki.jea@gmail.com>
2021-09-16 10:53:28 +0800

    Allow file upload and preview to blob

--- app/javascript/packs/trix-overrides.js
+++ app/javascript/packs/trix-overrides.js
@@ -1,13 +1,21 @@
 // prevents attachments:
-document.addEventListener("trix-file-accept", function (event) {
-  event.preventDefault()
-})
+// document.addEventListener("trix-file-accept", function (event) {
+//   event.preventDefault()
+// })
 
-document.addEventListener("turbolinks:load", () => {
-  const uploadButtons = document.querySelectorAll(".trix-button--icon-attach")
+// document.addEventListener("turbolinks:load", () => {
+//   const uploadButtons = document.querySelectorAll(".trix-button--icon-attach")
 
-  uploadButtons.forEach(uploadButton => {
-    uploadButton.disabled = true
-    uploadButton.parentElement.style.display = "none"
-  })
+//   uploadButtons.forEach(uploadButton => {
+//     uploadButton.disabled = true
+//     uploadButton.parentElement.style.display = "none"
+//   })
+// })
+
+window.addEventListener("trix-file-accept", function (event) {
+  const maxFileSize = 3000 * 3000 // around 9MB
+  if (event.file.size > maxFileSize) {
+    event.preventDefault()
+    alert("Only support attachment files upto size 9MB!")
+  }
 })
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module VideoSkwela
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # video previews for action_text
+    config.after_initialize do
+      ActionText::ContentHelper.allowed_attributes.add 'style'
+      ActionText::ContentHelper.allowed_attributes.add 'controls'
+
+      ActionText::ContentHelper.allowed_tags.add 'video'
+      ActionText::ContentHelper.allowed_tags.add 'source'
+    end
   end
 end
