267311067ab0e85f461b66a2dbc4f9bfe78dd985
saguiguilid <tbki.jea@gmail.com>
2021-09-20 11:05:42 +0800

    Feature: Course owner can delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,25 @@
 class CommentsController < ApplicationController
-  before_action :set_course, only: :create
-  before_action :set_lesson, only: :create
+  before_action :set_course, only: %i[create destroy]
+  before_action :set_lesson, only: %i[create destroy]
 
   def create
     @comment = @lesson.comments.build(comment_params)
     @comment.user = current_user
 
     if @comment.save
-      redirect_to course_lesson_path(@course, @lesson), notice: 'Comment has successfully been created.'
+      dom_id = 'comment_' + @comment.id.to_s
+      redirect_to course_lesson_path(@course, @lesson, anchor: dom_id),
+                  notice: 'Comment has successfully been created.'
     else
       redirect_to course_lesson_path(@course, @lesson), alert: @comment.errors.full_messages.to_sentence
     end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+
+    comment.destroy
+    redirect_to course_lesson_path(@course, @lesson), notice: 'Comment has successfully been destroyed.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
       get :analytics
     end
     resources :lessons do
-      resources :comments
+      resources :comments, only: %i[create destroy]
       put :sort
       member do
         delete :remove_video
