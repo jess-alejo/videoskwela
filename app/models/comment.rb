782e74c12c766293b502928ce30b2d2797413950
saguiguilid <tbki.jea@gmail.com>
2021-09-21 10:17:56 +0800

    Add public activity tracking for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :lesson
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
+
+  belongs_to :user, counter_cache: true
+  belongs_to :lesson, counter_cache: true
 
   validates :content, presence: true
 end
