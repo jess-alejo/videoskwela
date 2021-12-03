4b9dcb561a4fe374569ba64058a259c6422161d3
saguiguilid <tbki.jea@gmail.com>
2021-12-03 10:02:31 +0800

    Bootstrap template: part 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,32 +30,85 @@ import "./youtube"
 import "./videos"
 import "./selectize"
 
-// theme related
-// require( 'datatables.net-bs4' )();
-// require( 'datatables.net-autofill-bs4' )();
-// require( 'datatables.net-buttons-bs4' )();
-// require( 'datatables.net-colreorder-bs4' )();
-// require( 'datatables.net-responsive-bs4' )();
-// require( 'datatables.net-rowreorder-bs4' )();
-// require( 'datatables.net-searchPanes' )();
-// require( 'datatables.net-select-bs4' )();
-// import "datatables.net"
-import "datatables"
-// require("datatables.net")
-// import "datatables.net-autofill"
-// import "datatables.net-bs4"
-// require("datatables.net-buttons")()
-// require("datatables.net-colreorder")()
-// require("datatables.net-keytable")()
-// import "moment"
+// // theme related
+// // require( 'datatables.net-bs4' )();
+// // require( 'datatables.net-autofill-bs4' )();
+// // require( 'datatables.net-buttons-bs4' )();
+// // require( 'datatables.net-colreorder-bs4' )();
+// // require( 'datatables.net-responsive-bs4' )();
+// // require( 'datatables.net-rowreorder-bs4' )();
+// // require( 'datatables.net-searchPanes' )();
+// // require( 'datatables.net-select-bs4' )();
+// // import "datatables.net"
+// import "datatables"
+// // require("datatables.net")
+// // import "datatables.net-autofill"
+// // import "datatables.net-bs4"
+// // require("datatables.net-buttons")()
+// // require("datatables.net-colreorder")()
+// // require("datatables.net-keytable")()
+// // import "moment"
 
-import "autosize"
-import "bootstrap-notify"
-import "bootstrap-submenu"
+// // import "autosize"
+// // import "bootstrap-notify"
+// // import "bootstrap-submenu"
 
-import "./js/common"
-import "./js/scripts"
-import "./js/base/loader"
+// // import "./js/base/globals"
+
+// // import "./js/common"
+// // import "./js/scripts"
+// // import "./js/base/loader"
+
+
+
+
+
+// // <!-- Vendor Scripts Start -->
+// import "./js/vendor/jquery-3.5.1.min"
+// import "./js/vendor/bootstrap.bundle.min"
+// import "./js/vendor/OverlayScrollbars.min"
+// import "./js/vendor/autoComplete.min"
+// import "./js/vendor/clamp.min"
+
+// {/* <script src="icon/acorn-icons"
+// <script src="icon/acorn-icons-interface" */}
+// import "../icon/acorn-icons"
+// // import "../icon/acorn-icons"
+// // import "../icon/acorn-icons-interface"
+// // import "../icon/acorn-icons-learning"
+
+
+
+
+// import "./js/vendor/select2.full.min"
+
+// import "./js/vendor/fullcalendar/main.min"
+
+// import "./js/vendor/moment-with-locales.min"
+
+// import "./js/vendor/datepicker/bootstrap-datepicker.min"
+
+// import "./js/vendor/timepicker"
+
+// // <!-- Vendor Scripts End -->
+
+
+// // <!-- Template Base Scripts Start -->
+// import "./js/base/helpers"
+// import "./js/base/globals"
+// import "./js/base/nav"
+// import "./js/base/search"
+// import "./js/base/settings"
+// // <!-- Template Base Scripts End -->
+
+// // <!-- Page Specific Scripts Start -->
+// import "./js/cs/glide.custom"
+// import "./js/cs/charts.extend"
+// import "./js/pages/dashboard.elearning"
+
+// import "./js/common"
+// import "./js/scripts"
+// // <!-- Page Specific Scripts End -->
 
 
 
@@ -71,3 +124,47 @@ import "./js/base/loader"
 // OverlayScrollbars
 // quill.active
 // singleimageupload
+
+
+
+
+// <!-- Vendor Scripts Start -->
+import "./js/vendor/jquery-3.5.1.min"
+import "./js/vendor/bootstrap.bundle.min"
+import "./js/vendor/OverlayScrollbars.min"
+import "./js/vendor/autoComplete.min"
+import "./js/vendor/clamp.min"
+{/* <script src="icon/acorn-icons.js"></script> */}
+{/* <script src="icon/acorn-icons-interface.js"></script> */}
+{/* <script src="icon/acorn-icons-learning.js"></script> */}
+
+// import "../icon/acorn-icons"
+// import "../icon/acorn-icons-interface"
+// import "../icon/acorn-icons-learning"
+
+import "./js/vendor/glide.min"
+
+import "./js/vendor/Chart.bundle.min"
+
+import "./js/vendor/jquery.barrating.min"
+
+// <!-- Vendor Scripts End -->
+
+// <!-- Template Base Scripts Start -->
+import "./js/base/helpers"
+import "./js/base/globals"
+import "./js/base/nav"
+import "./js/base/search"
+import "./js/base/settings"
+// <!-- Template Base Scripts End -->
+// <!-- Page Specific Scripts Start -->
+
+import "./js/cs/glide.custom"
+
+import "./js/cs/charts.extend"
+
+import "./js/pages/dashboard.elearning"
+
+import "./js/common"
+import "./js/scripts"
+// <!-- Page Specific Scripts End -->
