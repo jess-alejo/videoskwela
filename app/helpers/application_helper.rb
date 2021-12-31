0e0aac0aca8fcef6a50b341e0a489afc3d8dae4d
saguiguilid <tbki.jea@gmail.com>
2021-12-31 10:40:50 +0800

    Apply Wizard Theme in Course Wizard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,4 +36,12 @@ module ApplicationHelper
       end
     end
   end
+
+  def space
+    # prevent haml whitespace being stripped in staging and production
+    # which would lead to elements being stuck together.
+    # As in development whitespace is not stripped, it looks good in
+    # development even without this helper.
+    " "
+  end
 end
